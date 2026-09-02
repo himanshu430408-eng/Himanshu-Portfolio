9b532d37c554dba40e242aca169fe675e0342715
Himanshu tiwari <himanshu430408@gmail.com>
2026-09-02 22:11:47 +0530

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -39,7 +39,7 @@ function initContact() {
         try {
 
             const response = await fetch(
-                "http://127.0.0.1:5000/api/contact",
+                "/api/contact",
                 {
                     method: "POST",
 
